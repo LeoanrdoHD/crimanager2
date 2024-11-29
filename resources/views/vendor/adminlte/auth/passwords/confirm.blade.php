95f46eb1ffd23c095789060b246ecc9483c5d453
Diego Humerez <diegohumerez84@gmail.com>
2024-11-29 11:36:32 -0400

    tercer sprint 1.1

--- resources/views/vendor/adminlte/auth/passwords/confirm.blade.php
+++ resources/views/vendor/adminlte/auth/passwords/confirm.blade.php
@@ -41,7 +41,7 @@
                 </div>
             @endif
 
-            <form method="POST" action="{{ route('password.confirm') }}"
+            <form method="POST" action="{{ route('password.confirm.custom') }}"
                   class="lockscreen-credentials @if(!config('adminlte.usermenu_image'))ml-0 @endif">
                 @csrf
 
